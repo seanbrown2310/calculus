729530cdfa04802b6570bc478591b258575faca9
Sean Brown <rc0029451@CRO-ICF07-08N>
2018-04-18 13:33:46 +0100

    Add project files.

---
+++ calculus/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace calculus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(695, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.openFileToolStripMenuItem.Text = "open ";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 488);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ calculus/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace calculus
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            public double time;
+            public double voltage;
+            public double current;
+            public double voltageDerivative;
+            public double charge;
+        }
+
+        List<row> table = new List<row>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+            {
+                openFileDialog1.FileName = "";
+                openFileDialog1.Filter = "CSV Files|*.csv";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                        {
+                            string line = sr.ReadLine();
+                            while (!sr.EndOfStream)
+                            {
+                                table.Add(new row());
+                                string[] l = sr.ReadLine().Split(',');
+                                table.Last().time = double.Parse(l[0]); 
+                                table.Last().voltage = double.Parse(l[0]);
+                                table.Last().current = double.Parse(l[1]);
+                            }
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show(openFileDialog1.FileName + " is not in the required format.");
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                    }
+                }
+            }
+
+        }
+    }
+}
